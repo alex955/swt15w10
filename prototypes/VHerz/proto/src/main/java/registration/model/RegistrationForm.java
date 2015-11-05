60e6e8da35f60a001b0a443f2d44067e9b585e61
VHerz <vinherz@gmx.de>
2015-11-05 17:17:25 +0100

    added registration prototype

---
+++ prototypes/VHerz/proto/src/main/java/registration/model/RegistrationForm.java
@@ -0,0 +1,35 @@
+package registration.model;
+
+/**
+ * Created by Vincenz on 30.10.15.
+ */
+public class RegistrationForm {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ prototypes/VHerz/proto/src/main/java/registration/model/UserRepository.java
@@ -0,0 +1,14 @@
+package registration.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by Vincenz on 27.10.15.
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> findByUsername(String username);
+
+
+}
