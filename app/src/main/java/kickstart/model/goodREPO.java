58ee7c6a7078ad4882d7bc8c27bd20cf3b2bd443
herzoga <herzog.alexander@mail.de>
2015-11-10 15:04:37 +0100

    Good activity Klassen hinzugefügt - pom.xml depencies gesetzt

---
+++ app/src/main/java/kickstart/model/goodREPO.java
@@ -0,0 +1,28 @@
+package kickstart.model;
+
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+
+public interface goodREPO extends CrudRepository<Good, Long> {
+	
+ 
+	
+ public void delete(Long id);
+ 
+ public long count();
+ 
+ public List<Good> findByname(String name);
+ 
+ public List<Good> findByLocation(String location);
+ 
+ public List<Good> findAll();
+ 
+
+ 
+
+ 
+}
