274f9e989eef1a513538230941690ad94e89c12f
andauh <andreashuber.eschenbach@web.de>
2015-11-12 16:38:00 +0100

    separate Controller hinzugefügt

--- app/src/main/java/kickstart/controller/CommonVariables.java
+++ app/src/main/java/kickstart/controller/CommonVariables.java
@@ -1,10 +1,12 @@
 package kickstart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import kickstart.model.CategoryRepo;
 import kickstart.model.UserRepository;
 
+@Controller
 public class CommonVariables {
     @Autowired
     protected UserRepository userRepository;
---
+++ app/src/main/java/kickstart/controller/SearchController.java
@@ -0,0 +1,5 @@
+package kickstart.controller;
+
+public class SearchController extends CommonVariables {
+
+}
