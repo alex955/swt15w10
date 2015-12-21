59c3b2ddf3c7fbfd6bc24fcf1d47c125d32941d4
VHerz <vinherz@gmx.de>
2015-12-21 15:19:18 +0100

    Verzweifelte Situationen erfordern verzweifelte Maßnahmen. Manueller EMail Merge, da git mich nicht mag.

--- app/src/main/java/kickstart/controller/SettingsController.java
+++ app/src/main/java/kickstart/controller/SettingsController.java
@@ -1,11 +1,6 @@
 package kickstart.controller;
 
-import kickstart.model.Article;
-import kickstart.model.ArticleRepo;
-import kickstart.model.CategoryFirstTierObject;
-import kickstart.model.User;
-import kickstart.model.UserRepository;
-import kickstart.model.UserSettings;
+import kickstart.model.*;
 import kickstart.utilities.CategoryMethods;
 
 import org.salespointframework.useraccount.UserAccount;
@@ -16,19 +11,17 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 import javax.validation.Valid;
-import javax.validation.constraints.Null;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -41,33 +34,42 @@ public class SettingsController {
     private final UserRepository userRepository;
 
     @Autowired
+    private final ValidatorRepository validatorRepository;
+
     private UserAccountManager userAccountManager;
-    
-	@Autowired private final CategoryMethods categoryMethods;
-	
-	@Autowired
-	private final ArticleRepo articleRepo;
+
+    @Autowired
+    private final CategoryMethods categoryMethods;
+
+    @Autowired
+    private final ArticleRepo articleRepo;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-    
-
-	protected LinkedList<CategoryFirstTierObject> processedCategories; 
 
     @Autowired
-    public SettingsController(ArticleRepo articleRepo, UserRepository userRepository, UserAccountManager userAccountManager, PasswordEncoder passwordEncoderkönn, CategoryMethods categoryMethods){
+    private final UserSettingsRepository userSettingsRepository;
+
+
+    protected LinkedList<CategoryFirstTierObject> processedCategories;
+
+    @Autowired
+    public SettingsController(ArticleRepo articleRepo, UserRepository userRepository, UserAccountManager userAccountManager, PasswordEncoder passwordEncoder, CategoryMethods categoryMethods, ValidatorRepository validatorRepository, UserSettingsRepository userSettingsRepository){
         this.userRepository = userRepository;
         this.userAccountManager= userAccountManager;
         this.passwordEncoder = passwordEncoder;
         this.categoryMethods = categoryMethods;
         this.articleRepo = articleRepo;
+        this.validatorRepository = validatorRepository;
+        this.userSettingsRepository = userSettingsRepository;
     }
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value ="/usersettings")
-    public String changeSettings(@ModelAttribute("UserSettings") @Valid UserSettings userSettings, @LoggedIn Optional<UserAccount> userAccount, Model model){
+    public String changeSettings(@ModelAttribute("UserSettingsForm") UserSettingsForm userSettingsForm, @LoggedIn Optional<UserAccount> userAccount, Model model){
 
         User user = userRepository.findByUserAccount(userAccount.get());
+        UserSettings userSettings = new UserSettings();
 
         this.processedCategories = categoryMethods.getProcessedCategories();
         model.addAttribute("categories", this.processedCategories);
@@ -78,114 +80,102 @@ public class SettingsController {
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/usersettings", method = RequestMethod.POST)
-    public String saveSettings(@LoggedIn Optional<UserAccount> userAccount, @ModelAttribute("UserSettings") @Valid UserSettings userSettings, BindingResult result, Model model) throws AddressException, MessagingException {
+    public String saveSettings(@ModelAttribute("UserSettingsForm") @Valid UserSettingsForm userSettingsForm, BindingResult result, @LoggedIn Optional<UserAccount> userAccount,  Model model) throws AddressException, MessagingException {
 
         User user = userRepository.findByUserAccount(userAccount.get());
+        UserSettings userSettings = new UserSettings();
+
+        model.addAttribute("userSettings", new UserSettings());
+        model.addAttribute("user", user);
 
         this.processedCategories = categoryMethods.getProcessedCategories();
         model.addAttribute("categories", this.processedCategories);
-        model.addAttribute("user", user);
+
+        if(userSettingsRepository.findByUserId(user.getId()) != null){
+            userSettingsRepository.delete(userSettingsRepository.findByUserId(user.getId()).getId());
+        }
 
         if(result.hasErrors())
             return "usersettings";
 
-        // System.out.println(user);
+        userSettings.setUserId(user.getId());
 
         //Adressänderung
-        if(!userSettings.getNewCity().isEmpty())
-        user.setCity(userSettings.getNewCity());
+        if(!userSettingsForm.getNewCity().isEmpty())
+            user.setCity(userSettingsForm.getNewCity());
 
-        if(!userSettings.getNewZip().isEmpty())
-        user.setZip(userSettings.getNewZip());
+        if(!userSettingsForm.getNewZip().isEmpty())
+            user.setZip(userSettingsForm.getNewZip());
 
-        if(!userSettings.getNewStreetName().isEmpty())
-        user.setStreetName(userSettings.getNewStreetName());
+        if(!userSettingsForm.getNewStreetName().isEmpty())
+            user.setStreetName(userSettingsForm.getNewStreetName());
 
-        if(!userSettings.getNewHouseNumber().isEmpty())
-        user.setHouseNumber(userSettings.getNewHouseNumber());
+        if(!userSettingsForm.getNewHouseNumber().isEmpty())
+            user.setHouseNumber(userSettingsForm.getNewHouseNumber());
 
-        if(!userSettings.getNewAddressAddition().isEmpty())
-        user.setAddressAddition(userSettings.getNewAddressAddition());
+        if(!userSettingsForm.getNewAddressAddition().isEmpty())
+            user.setAddressAddition(userSettingsForm.getNewAddressAddition());
 
         //Email-Änderung
+        if(!userSettingsForm.getNewEmail().isEmpty()) {
 
-        //TODO: Email Confirmation
+            Validator validator = new Validator(user, 3);
+            validatorRepository.save(validator);
+            userSettings.setNewEmail(userSettingsForm.getNewEmail());
 
-        if(!userSettings.getNewEmail().isEmpty())
-        user.setEmail(userSettings.getNewEmail());
+            EMailController.sendEmail(user.getEmail(), validator.getToken(), validator.getUsage());
+
+        }
 
         //Passwort-Änderung
-        if(!userSettings.getNewPassword().isEmpty()) {
+        if(!userSettingsForm.getNewPassword().isEmpty()) {
 
-            /* CONTROL OUTPUT
-            System.out.println();
-            System.out.println("Altes PW Eingabe: " + userSettings.getOldPassword());
-            System.out.println("Altes PW verschlüsselt: " + passwordEncoder.encode(userSettings.getOldPassword()));
-            System.out.println("Altes PW von UserAccount:  " + user.getUserAccount().getPassword().toString());
-            System.out.println("Altes PW = UserAccount altes PW? " + passwordEncoder.matches(userSettings.getOldPassword(), user.getUserAccount().getPassword().toString()));
-            System.out.println();
-            System.out.println("Neues PW: " + userSettings.getNewPassword());
-            System.out.println("ConfirmPW: " + userSettings.getConfirmPW());
-            System.out.println("Neues PW = confirmPW? " + userSettings.getNewPassword().equals(userSettings.getConfirmPW()));
-            System.out.println();
-            */
-
-            if (passwordEncoder.matches(userSettings.getOldPassword(), userAccount.get().getPassword().toString()) && userSettings.getNewPassword().equals(userSettings.getConfirmPW())) {
-                userAccountManager.changePassword(userAccount.get(), userSettings.getNewPassword());
-                //System.out.println("PW geändert");
-                //System.out.println();
+            if (passwordEncoder.matches(userSettingsForm.getOldPassword(), userAccount.get().getPassword().toString()) && userSettingsForm.getNewPassword().equals(userSettingsForm.getConfirmPW())) {
+                userAccountManager.changePassword(userAccount.get(), userSettingsForm.getNewPassword());
 
             } else {
-
-               // System.out.println("PW nicht geändert");
-               // System.out.println();
-
                 return "usersettings";
             }
         }
 
         //Sprachenänderung
-        if(userSettings.getNewLanguage1().equals("null")){
+        if(userSettingsForm.getNewLanguage1().equals("null")){
             user.setLanguage1(null);
         } else {
-            user.setLanguage1(userSettings.getNewLanguage1());
+            user.setLanguage1(userSettingsForm.getNewLanguage1());
         }
 
-        if(userSettings.getNewLanguage2().equals("null")){
+        if(userSettingsForm.getNewLanguage2().equals("null")){
             user.setLanguage2(null);
         } else {
-            user.setLanguage2(userSettings.getNewLanguage2());
+            user.setLanguage2(userSettingsForm.getNewLanguage2());
         }
 
-        if(userSettings.getNewLanguage3().equals("null")){
+        if(userSettingsForm.getNewLanguage3().equals("null")){
             user.setLanguage3(null);
         } else {
-            user.setLanguage3(userSettings.getNewLanguage3());
+            user.setLanguage3(userSettingsForm.getNewLanguage3());
         }
 
 
         userAccountManager.save(user.getUserAccount());
+        userSettingsRepository.save(userSettings);
         userRepository.save(user);
 
-        // System.out.println(user);
-
-        return "redirect:/search";
+        return "usersettings";
     }
-    
+
     @RequestMapping(value = "/deleteuser")
-    public String deleteUser(@LoggedIn Optional<UserAccount> userAccount) {
-    	long userId = this.userRepository.findByUserAccount(userAccount.get()).getId();
-    	User currentUser = this.userRepository.findOne(userId);
-        
-        List<Article> userArticles = this.articleRepo.findByCreator(currentUser);
-        this.articleRepo.delete(userArticles);
-        
-        this.userAccountManager.disable(this.userRepository.findOne(userId).getUserAccount().getIdentifier());
-        userRepository.delete(userRepository.findByUserAccount(userAccount.get()));
-        
+    public String deleteUser(@LoggedIn Optional<UserAccount> userAccount, ModelMap modelMap) throws AddressException, MessagingException {
+        User user = userRepository.findByUserAccount(userAccount.get());
+
+        Validator validator = new Validator(user, 2);
+        validatorRepository.save(validator);
+
+        EMailController.sendEmail(user.getEmail(), validator.getToken(), 2);
         return "redirect:/logout";
     }
-    
+
 
 
 }
---
+++ app/src/main/java/kickstart/model/Validator.java
@@ -0,0 +1,73 @@
+package kickstart.model;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.persistence.*;
+import java.util.Random;
+
+/**
+ * Created by Vincenz on 07.12.15.
+ */
+@Entity
+public class Validator {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    @OneToOne
+    private User user;
+
+    private String token;
+    private int usage;
+
+    public Validator(User user, int usage) throws AddressException, MessagingException {
+        super();
+        this.user = user;
+        this.token = generateToken(32);
+        this.usage = usage;
+    }
+
+    public Validator() {
+        super();
+    }
+
+    public static String generateToken(int length)
+    {
+        String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random rng = new Random();
+
+        char[] text = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            text[i] = characters.charAt(rng.nextInt(characters.length()));
+        }
+        return new String(text);
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public int getUsage() {
+        return usage;
+    }
+
+    public void setUsage(int usage) {
+        this.usage = usage;
+    }
+
+
+}
