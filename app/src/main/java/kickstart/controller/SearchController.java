1adff0e8ff04f96bab982ae4bcc472cad0b2fdf1
herzoga <herzog.alexander@mail.de>
2016-01-04 15:04:01 +0100

    Tag v2.1.0
    Tagsuche in den Kategorien. Erste Variante
    Theoretisch funktioniert sie aber praktisch nicht.
    ContainsAll() funktioniert nicht.
    @andauh guckst drüber?
    @alex955 kannst ja mal deine CSS Fähigkeiten austoben^

--- app/src/main/java/kickstart/controller/SearchController.java
+++ app/src/main/java/kickstart/controller/SearchController.java
@@ -2,12 +2,16 @@ package kickstart.controller;
 
 
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.salespointframework.useraccount.UserAccount;
 import org.salespointframework.useraccount.web.LoggedIn;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,13 +21,106 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kickstart.model.Category;
+import kickstart.model.CategoryFirstTierObject;
+import kickstart.model.CategoryRepo;
+import kickstart.model.NewAttributes;
 import kickstart.model.SearchQuery;
 import kickstart.model.User;
+import kickstart.model.UserRepository;
 import kickstart.model.Article;
+import kickstart.model.ArticleRepo;
+import kickstart.model.Attribute;
 
 
 @Controller
-public class SearchController extends CommonVariables {
+@Scope("session")
+public class SearchController {
+	
+	 @Autowired
+	    private UserRepository userRepository;
+	    
+		@Autowired
+		private CategoryRepo categories;
+		
+		@Autowired
+		private ArticleRepo articleRepo;
+		
+		private long current_cat=0;
+		
+
+		protected LinkedList<CategoryFirstTierObject> processedCategories; 
+		
+		protected LinkedList<CategoryFirstTierObject> getProcessedCategories(){
+			LinkedList<CategoryFirstTierObject> toReturn = new LinkedList<CategoryFirstTierObject>();
+			Iterable<Category> foundCategories = categories.findAll();
+			
+			for(Category s : foundCategories){
+				if(s.getRoot()) {
+					LinkedList<Category> subcats = new LinkedList<Category>();
+					
+					
+					for(Category t : foundCategories){
+						if(t.getPredecessor() == s.getId()){
+							subcats.add(t);
+						}
+					}
+					
+					CategoryFirstTierObject toAdd = new CategoryFirstTierObject(s, subcats);
+					toReturn.add(toAdd);
+				}
+				else{
+				}
+			}
+			return toReturn;
+		}
+		
+		public Map<String, List<Category>> getCategoryMap(){
+			Map<String, List<Category>> toReturn = new HashMap<String, List<Category>>();
+			Iterable<Category> allCategories = this.categories.findAll();
+			
+			for(Category s : this.categories.findAll()){
+				long currCat = s.getId();
+				LinkedList<Category> putList = getChildren(currCat);
+				toReturn.put(s.getName(), putList);
+			}
+			
+			return toReturn;
+		}
+		
+		public LinkedList<Category> getChildren(long id){
+			LinkedList<Category> toReturn = new LinkedList<Category>();
+			for(Category c : this.categories.findAll()){
+				if(c.getPredecessor() == id) toReturn.add(c);
+			}
+			
+			return toReturn;
+			
+		}
+
+		public Model getCurrent_cat(Model model) {
+			if (current_cat==0) model.addAttribute("current_category",new Category("Alle Kategorien", 0));
+			else model.addAttribute("current_category",this.categories.findOne(current_cat).get());
+			
+			return model;
+		}
+
+		public void setCurrent_cat(long current_cat) {
+			if (this.categories.findOne(current_cat).get().getId()==current_cat) this.current_cat = current_cat; 
+			else throw new IllegalArgumentException("Kategorie ID existiert nicht!");
+		}
+		
+		public Model cat_exchange(Model model){
+			if (current_cat==0) model.addAttribute("current_category",new Category("Alle Kategorien", 0));
+			else model.addAttribute("current_category",this.categories.findOne(current_cat).get());
+			
+			return model;
+			
+		}
+		
+		public Long getCurrent_cat(){
+				return current_cat;	
+				}
+		
 			
 	
 	public List<Article> getAllSubcategoryItems(long subcatId){
@@ -49,7 +146,11 @@ public class SearchController extends CommonVariables {
 		return this.articleRepo.findByCategory(subcatID);
 	}
 	
-	   //@PreAuthorize("isAuthenticated()")
+	   
+	
+		//Controller
+	
+		//@PreAuthorize("isAuthenticated()")
 		@RequestMapping(value = "/search", method = RequestMethod.POST)
 	    public String searcharticle(@ModelAttribute("SearchQuery") SearchQuery SearchQuery, Model model ) {
 		
@@ -58,6 +159,8 @@ public class SearchController extends CommonVariables {
 			model.addAttribute("categories", this.processedCategories);
 			model.addAttribute("categoriesForm", this.categories.findAll());
 			model=this.getCurrent_cat(model);
+			model.addAttribute("FormAttributes",this.categories.findOne(articleRepo.findOne(this.getCurrent_cat()).getCategory()).get().getAttributes());
+			model.addAttribute("NewAttributes",new NewAttributes());
 			SearchQuery.setCategory(this.getCurrent_cat());
 			
 			System.out.println("Gesucht "+ SearchQuery.getQuery() +" in "+SearchQuery.getCategory());
@@ -106,7 +209,8 @@ public class SearchController extends CommonVariables {
 			this.processedCategories = this.getProcessedCategories();
 			model.addAttribute("categories", this.processedCategories);
 			model.addAttribute("anzeigen", catGoods);
-			
+			model.addAttribute("NewAttributes",new NewAttributes());
+			model.addAttribute("FormAttributes",this.categories.findOne(catID).get().getAttributes());
 			model=this.getCurrent_cat(model);
 			
 			
@@ -124,6 +228,8 @@ public class SearchController extends CommonVariables {
 			this.processedCategories = this.getProcessedCategories();
 			model.addAttribute("categories", this.processedCategories);
 			model.addAttribute("anzeigen", articles);
+			model.addAttribute("FormAttributes",this.categories.findOne(articleRepo.findOne(this.getCurrent_cat()).getCategory()).get().getAttributes());
+			model.addAttribute("NewAttributes",new NewAttributes());
 			
 			model=this.getCurrent_cat(model);
 			
@@ -131,7 +237,71 @@ public class SearchController extends CommonVariables {
 			return "search";
 			
 			}
+			
+			
+			@RequestMapping(value = "/searchbytags", method = RequestMethod.POST)
+		    public String searchbytags(@ModelAttribute("searchattributes") NewAttributes SearchTag, Model model ) {
+				SearchQuery SearchQuery= new SearchQuery();
+				
+				this.processedCategories = this.getProcessedCategories();
+				model.addAttribute("categories", this.processedCategories);
+				model.addAttribute("categoriesForm", this.categories.findAll());
+				model.addAttribute("FormAttributes",this.categories.findOne(articleRepo.findOne(this.getCurrent_cat()).getCategory()).get().getAttributes());
+				model.addAttribute("NewAttributes",new NewAttributes());
+				model=this.getCurrent_cat(model);
+				SearchQuery.setCategory(this.getCurrent_cat());
+				
+				System.out.println("Gesucht "+ SearchQuery.getQuery() +" in "+SearchQuery.getCategory());
+				List<Article> catGoods = new LinkedList<Article>();
+				
+				if (SearchQuery.getCategory()==0) { catGoods = this.articleRepo.findAll() ;}
+						else { catGoods = articleRepo.findByCategory(SearchQuery.getCategory()); }
+				System.out.println("Length of list: " + catGoods.size());
+				 
+				List<Article> output = new LinkedList<Article>();
+				List<String> providedtags =  SearchTag.getChoosenTags();
+				List<Attribute> searchattributes = new LinkedList<Attribute>();
+				
+		 		   
+				
+				// Übermittelte leere Attribute aussortieren
+				int count=0;
+				for(String e:providedtags){
+					if (e.isEmpty()==true) {
+					count++;	
+					} 
+					else
+					{ 
+					LinkedList<String> tag = new LinkedList<String>();
+					tag.add(e);
+					// fügt das Attribut der Attribute Liste hinzu
+					Attribute att = new Attribute();
+					att.setName(this.categories.findOne(this.getCurrent_cat()).get().getAttributes().get(count).getName());
+					att.setTags(tag);
+					searchattributes.add(att);
+					count++;
+					}
+				 } 
+				
+				System.out.println(searchattributes);
+				
+				Boolean flag;
+				//Attribute mit Artikeln vergleichen
+				for(Article good:catGoods) {
+						System.out.println(good.getAttributes());
+						System.out.println(good.getAttributes().containsAll(searchattributes));
+						if (good.getAttributes().containsAll(searchattributes)) output.add(good);
+				}
+			
+				   
+				   model.addAttribute("anzeigen", output);	
+				   
+				   return "search";
+				   
+			}
 			 
+			
+			
 			 
 			
 	
