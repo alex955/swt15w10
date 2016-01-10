99fe7c37c27a5b10cbcdccf3d5d47da1cc8668c8
andauh <andreashuber.eschenbach@web.de>
2016-01-10 13:31:22 +0100

    Revert "fixed edit article"
    This reverts commit fab03a84318cfedf2959039ced5ded646b7c102c.

--- app/src/main/java/refugeeApp/controller/ArticleController.java
+++ app/src/main/java/refugeeApp/controller/ArticleController.java
@@ -140,7 +140,6 @@ public class ArticleController {
 		model.addAttribute("categories", this.processedCategories);
 		model.addAttribute("categoriesForm", this.categories.findAll());
 		model.addAttribute("currentUserId", currentUserId);
-		model.addAttribute("userId", currentUserId);
 		model.addAttribute("user", this.userRepository.findOne(currentUserId));
 		model.addAttribute("Creator", articleRepo.findOne(id).getCreator());
 		model.addAttribute("Article", originalArticle);
@@ -165,11 +164,9 @@ public class ArticleController {
 				if (result.hasFieldErrors("title")) {
 					final String titleError = language.getTitleError();
 					modelMap.addAttribute("titleError", titleError);
-					return "editArticle";
 				}
 			}
-			//kind errors cannot happen, because an hidden argument (initialised with prior kind) is passed, kind cannot be changed
-			//return "editArticle";
+			return "editArticle";
 		}
 
 		//case: current user didnt create article && logged in user no admin -> end
