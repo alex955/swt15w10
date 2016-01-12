15e11dd8cfe0234ed8c600341f38cbbbc9013497
Justus Adam <dev@justus.science>
2016-01-12 23:41:38 +0100

    Fixed a merge error

--- app/src/main/java/refugeeApp/utilities/PossibleChatMessages.java
+++ app/src/main/java/refugeeApp/utilities/PossibleChatMessages.java
@@ -31,96 +31,6 @@ public class PossibleChatMessages {
 	private Map<Integer,String> possibleChatMessage = new HashMap<Integer, String>();
 	
 	/**
-	 * Gets the possible start messages.
-	 *
-	 * @return Mapped (by int id) String values for possible messages with which a conversation can be started out of an article view
-	 */
-	public Map<Integer,String> getPossibleStartMessages(String kind) {
-		Map<Integer,String> toReturn = new HashMap<Integer,String>();
-
-		int[] startMessages = { };
-
-		if(kind.equals(ConfigurationMisc.ACTIVITY)) startMessages = this.startMessagesActivity;
-		if(kind.equals(ConfigurationMisc.ARTICLE)) startMessages = this.startMessagesArticle;
-
-		for(int i = 0; i < startMessages.length; i++){
-			for(Map.Entry<Integer, String> s : possibleChatMessage.entrySet()){
-				if(s.getKey() == startMessages[i]) {
-					toReturn.put(startMessages[i], s.getValue());
-				}
-			}
-		}
-
-		return toReturn;
-	}
-
-	/**
-	 * Gets the possible messages from starter.
-	 *
-	 * @return Mapped (by int id) String values for possible messages with which a conversation can be appended by interested party, conversation already exists
-	 */
-	public Map<Integer, String> getPossibleMessagesFromStarter(String kind){
-		Map<Integer,String> toReturn = new HashMap<Integer,String>();
-
-		int[] possibleMessagesFromStarter = {};
-
-		if (kind.equals(ConfigurationMisc.ACTIVITY))
-			possibleMessagesFromStarter = this.possibleMessagesFromStarterActivity;
-		if (kind.equals(ConfigurationMisc.ARTICLE))
-			possibleMessagesFromStarter = this.possibleMessagesFromStarterArticle;
-		
-		for(int i = 0; i < possibleMessagesFromStarter.length; i++){
-			for(Map.Entry<Integer, String> s : possibleChatMessage.entrySet()){
-				if(s.getKey() == possibleMessagesFromStarter[i]) {
-					toReturn.put(possibleMessagesFromStarter[i], s.getValue());
-				}
-			}
-		}
-		
-		return toReturn;
-	}
-	
-	/**
-	 * Gets the possible answers to message.
-	 *
-	 * @param key ID of message
-	 * @return Mapped (by int id) String values for possible answers to a certain message
-	 */
-	public Map<Integer, String> getPossibleAnswersToMessage(int key) {
-		Map<Integer, String> toReturn = new HashMap<Integer, String>();
-		
-		LinkedList<Integer> possibleAnswerKeys = this.possibleAnswersToMessage.get(key);
-		
-		for(int s : possibleAnswerKeys){
-			toReturn.put(s, this.possibleChatMessage.get(s));
-		}
-		
-		return toReturn;
-	}
-	
-	/**
-	 * Gets the possible chat messages.
-	 *
-	 * @return all possible chat messages, all questions and answers
-	 */
-	public Map<Integer,String> getPossibleChatMessages() {
-		return possibleChatMessage;
-	}
-	
-	/**
-	 * Gets a certain text block by id.
-	 *
-	 * @param id Textblock ID
-	 * @return Map with one text block - mapped by it's id. Map needed for further computational magic.
-	 */
-	public Map<Integer, String> getCertainTextBlock(int id){
-		Map<Integer, String> toReturn = new HashMap<Integer, String>();
-		toReturn.put(id, this.possibleChatMessage.get(id));
-		
-		return toReturn;
-	}
-	
-	/**
 	 * Constructor, contains hardcoded chat flow.
 	 */
 	//wasn't supposed to be so heavily hardcoded,..
@@ -176,7 +86,7 @@ public class PossibleChatMessages {
 		this.possibleChatMessage.put(16, "textblock.activityWontTakePlace");
 		LinkedList<Integer> answersToSixteen = new LinkedList<Integer>();
 
-		this.possibleChatMessage.put(17 ,"textblock.canYouSendArticle");
+		this.possibleChatMessage.put(17, "textblock.canYouSendArticle");
 		LinkedList<Integer> answersToSeventeen = new LinkedList<Integer>();
 
 		answersToZero.add(0);
@@ -265,14 +175,19 @@ public class PossibleChatMessages {
 //		this.possibleChatMessage.put(15,"Halodrieu");
 
 	}
-	
+
 	/**
 	 * Gets the possible start messages.
 	 *
 	 * @return Mapped (by int id) String values for possible messages with which a conversation can be started out of an article view
 	 */
-	public Map<Integer, String> getPossibleStartMessages() {
-		Map<Integer, String> toReturn = new HashMap<Integer, String>();
+	public Map<Integer, String> getPossibleStartMessages(String kind) {
+		Map<Integer,String> toReturn = new HashMap<Integer,String>();
+
+		int[] startMessages = {};
+
+		if (kind.equals(ConfigurationMisc.ACTIVITY)) startMessages = this.startMessagesActivity;
+		if (kind.equals(ConfigurationMisc.ARTICLE)) startMessages = this.startMessagesArticle;
 
 		for (int startMessage : startMessages) {
 			possibleChatMessage.entrySet().stream().filter(s -> s.getKey() == startMessage).forEach(s -> toReturn.put(startMessage, s.getValue()));
@@ -280,14 +195,21 @@ public class PossibleChatMessages {
 
 		return toReturn;
 	}
-
+	
 	/**
 	 * Gets the possible messages from starter.
 	 *
 	 * @return Mapped (by int id) String values for possible messages with which a conversation can be appended by interested party, conversation already exists
 	 */
-	public Map<Integer, String> getPossibleMessagesFromStarter() {
+	public Map<Integer, String> getPossibleMessagesFromStarter(String kind) {
 		Map<Integer, String> toReturn = new HashMap<Integer, String>();
+
+		int[] possibleMessagesFromStarter = {};
+
+		if (kind.equals(ConfigurationMisc.ACTIVITY))
+			possibleMessagesFromStarter = this.possibleMessagesFromStarterActivity;
+		if (kind.equals(ConfigurationMisc.ARTICLE))
+			possibleMessagesFromStarter = this.possibleMessagesFromStarterArticle;
 
 		for (int aPossibleMessagesFromStarter : possibleMessagesFromStarter) {
 			possibleChatMessage.entrySet().stream().filter(s -> s.getKey() == aPossibleMessagesFromStarter).forEach(s -> toReturn.put(aPossibleMessagesFromStarter, s.getValue()));
@@ -295,7 +217,7 @@ public class PossibleChatMessages {
 
 		return toReturn;
 	}
-
+	
 	/**
 	 * Gets the possible answers to message.
 	 *
@@ -313,7 +235,7 @@ public class PossibleChatMessages {
 
 		return toReturn;
 	}
-
+	
 	/**
 	 * Gets the possible chat messages.
 	 *
@@ -322,7 +244,7 @@ public class PossibleChatMessages {
 	public Map<Integer, String> getPossibleChatMessages() {
 		return possibleChatMessage;
 	}
-
+	
 	/**
 	 * Gets a certain text block by id.
 	 *
@@ -335,7 +257,7 @@ public class PossibleChatMessages {
 
 		return toReturn;
 	}
-
+	
 	/**
 	 * returns the "free chat" texblock.
 	 *
